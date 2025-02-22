c49bfeff2552a574f5c25cfdabeb014719c25b0b
TommasoQ <158309518+TommasoQ@users.noreply.github.com>
2025-02-22 17:17:04 +0100

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) "is compiling SYCL_Hungarian_Algorithm.cpp"
-icpx -o3 -fsycl SYCL_Hungarian_Algorithm.cpp -o SYCL_Hungarian_Algorithm
-if [ $? -eq 0 ]; then ./SYCL_Hungarian_Algorithm; fi
+icpx -o3 -fsycl ./src/SYCL_Hungarian_Algorithm.cpp -o ./src/SYCL_Hungarian_Algorithm
+if [ $? -eq 0 ]; then ./src/SYCL_Hungarian_Algorithm; fi
 
